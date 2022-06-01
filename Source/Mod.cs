b57cbd2d04158e14fa9283ef5019faaaa954503c
William So <polyipseity@gmail.com>
2022-06-01 19:53:13 +0800

    Format all files
    Signed-off-by: William So <polyipseity@gmail.com>

--- Source/Mod.cs
+++ Source/Mod.cs
@@ -1,14 +1,17 @@
 using Verse;
 
-namespace VARIABLE_NAMESPACE {
-	public class EarlyModStartup : Mod {
-		public EarlyModStartup(ModContentPack content) : base(content) {}
+namespace VARIABLE_NAMESPACE
+{
+	public class EarlyModStartup : Mod
+	{
+		public EarlyModStartup(ModContentPack content) : base(content) { }
 	}
 
 	[StaticConstructorOnStartup]
-	public static class ModStartup {
+	public static class ModStartup
+	{
 		public const string ID = nameof(VARIABLE_NAMESPACE);
 
-		static ModStartup() {}
+		static ModStartup() { }
 	}
 }
